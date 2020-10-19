3c71d0ddf5266ab3a36a3122938d3a45df910f93
Vitalik Prac <mastersasori007@gmail.com>
2020-10-20 01:09:57 +0300

    Updates

---
+++ precache-manifest.0962178912dc1149f1b8308255b0e8ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59165beee5323bf086007fb50ce01125",
+    "url": "/dolab2/index.html"
+  },
+  {
+    "revision": "2eab22defcd79b08056f",
+    "url": "/dolab2/static/css/2.5e6cee6f.chunk.css"
+  },
+  {
+    "revision": "ea48d89844be0d125df6",
+    "url": "/dolab2/static/css/main.f7a18c83.chunk.css"
+  },
+  {
+    "revision": "2eab22defcd79b08056f",
+    "url": "/dolab2/static/js/2.b8b6a142.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/dolab2/static/js/2.b8b6a142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea48d89844be0d125df6",
+    "url": "/dolab2/static/js/main.9c37a78d.chunk.js"
+  },
+  {
+    "revision": "f70017ad6ac3ac3653e8",
+    "url": "/dolab2/static/js/runtime-main.1d604486.js"
+  }
+]);
